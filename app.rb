fddbc0cbf6adcbc69331b489b46aba0542bb5d2c
Giorgio <giorgioce@outlook.com>
2020-10-30 11:24:22 +0000

    Player inputs name

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra/base'
 class RockPaperScissors < Sinatra::Base
-  get '/test' do
-    'test page'
+  # get '/test' do
+  #   'test page'
+  # end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/player' do
+    @player = params[:player]
+    erb :play
   end
 
   run! if app_file == $0
